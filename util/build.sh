cc9b889850848323dc7dea40c3d8584eb83141ed
Andrey Petrov <andrey.petrov@shazow.net>
2010-12-19 18:17:52 -0800

    Chrome store related stuff.

--- util/build.sh
+++ util/build.sh
@@ -9,11 +9,14 @@ BUILD_PATH=../build/linerage
 WORKING_PATH=../static
 
 rm -rf $BUILD_PATH
-mkdir -p $BUILD_PATH/{js,css,levels}
+mkdir -p $BUILD_PATH/{js,css,levels,images}
 cp -vr $WORKING_PATH/js/extern $BUILD_PATH/js/extern
 cp -v $WORKING_PATH/built.html $BUILD_PATH/index.html
+cp -v $WORKING_PATH/manifest.json $BUILD_PATH/manifest.json
 cp -vr $WORKING_PATH/css/* $BUILD_PATH/css/
+cp -vr $WORKING_PATH/images/* $BUILD_PATH/images/
 cp -vr $WORKING_PATH/levels/* $BUILD_PATH/levels/
+mv $BUILD_PATH/images/icon*.png $BUILD_PATH/
 
 echo "Compiling..."
 
@@ -29,3 +32,8 @@ java -jar compiler.jar \
     --js ../static/js/core/Hud.js \
     --js ../static/js/core/Game.js \
     --js ../static/js/core/Init.js \
+
+cd $BUILD_PATH
+rm linerage.zip
+zip -r ../linerage.zip .
+cd -
