dca21b680cba1fc01fff79b0180c6d324e2e84c5
Andrey Petrov <andrey.petrov@shazow.net>
2010-12-26 19:53:03 -0800

    Starting refactor to class-based code.

---
+++ static/js/lib/Clock.js
@@ -0,0 +1,19 @@
+var Clock = Class({
+    init: function() {
+        this.num_ticks = 0;
+        this.time_ticked = Clock.now;
+    },
+    tick: function() {
+        var delta = Clock.now - this.time_ticked;
+        this.time_ticked = Clock.now;
+        this.num_ticks++;
+        return delta;
+    },
+    delta: function() {
+        return Clock.now - this.time_ticked;
+    }
+});
+Clock.update = function() {
+    Clock.now = +new Date();
+}
+Clock.update();
